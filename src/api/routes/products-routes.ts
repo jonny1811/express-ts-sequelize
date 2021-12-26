ebb193ad626066b5f426681e82cacf4feb87d8fb
jonny1811 <jonny@quanto.la>
2021-12-25 23:46:55 -0300

    feat: add new controller, router and validators to products

--- src/api/routes/products-routes.ts
+++ src/api/routes/products-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import Middleware from '../../db/middleware';
+import ProductsValidator from '../../db/validators/products.validator';
 import ProductsController from '../controller/products';
 
 const productsBaseUrl = '/products';
@@ -7,6 +8,36 @@ const productsBaseUrl = '/products';
 export default (router: Router): void => {
     router.post(
         `${productsBaseUrl}/create`,
+        ProductsValidator.checkCreateProducts(),
         Middleware.handleValidationError,
+        ProductsController.create
+    );
+
+    router.get(
+        `${productsBaseUrl}`,
+        ProductsValidator.checkGetProducts(),
+        Middleware.handleValidationError,
+        ProductsController.getRecordsWithPagination
+    );
+
+    router.get(
+        `${productsBaseUrl}/:id`,
+        ProductsValidator.checkIdParam(),
+        Middleware.handleValidationError,
+        ProductsController.getByID
+    );
+
+    router.put(
+        `${productsBaseUrl}/update/:id`,
+        ProductsValidator.checkIdParam(),
+        Middleware.handleValidationError,
+        ProductsController.update
+    );
+
+    router.delete(
+        `${productsBaseUrl}/delete/:id`,
+        ProductsValidator.checkIdParam(),
+        Middleware.handleValidationError,
+        ProductsController.delete
     )
 }
